16ffe2831aabb431890d374bd625ad8928f9d732
GabrielDaloco <daloco.gabriel@gmail.com>
2023-09-20 11:47:33 -0300

    Projeto Finalizado

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -6,8 +6,32 @@ import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
+import { useState } from 'react';
+import { useCompletion } from 'ai/react'
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const {
+    input,
+    setInput,
+    handleInputChange,
+    handleSubmit,
+    completion,
+    isLoading
+  } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+
+    },
+    headers:{
+      'Content-Type': 'application/json'
+    }
+  })
 
   return (
     <div className='min-h-screen flex flex-col'>
@@ -34,11 +58,14 @@ export function App() {
             <Textarea 
               className='resize-none p-4 leading-relaxed'
               placeholder='Inclua o prompt para a IA...'
+              value={input}
+              onChange={handleInputChange}
             />
             <Textarea 
               className='resize-none p-4 leading-relaxed'
               placeholder='Resultado gerado pela IA...' 
               readOnly
+              value={completion}
             />
           </div>
           <p className='text-sm text-muted-foreground'>Lembre-se: você pode utilizar a variável <code className='text-violet-400'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado</p>
@@ -46,24 +73,15 @@ export function App() {
 
 
         <aside className='w-80 space-y-6'>
-          <VideoInputForm/> {/* É o formulário do vídeo sendo colocado aqui */}
+          <VideoInputForm onVideoUploaded={setVideoId}/> {/* É o formulário do vídeo sendo colocado aqui */}
 
           <Separator/>
 
-          <form className='space-y-6'>
+          <form onSubmit={handleSubmit} className='space-y-6'>
             
             <div className='space-y-1'>
                 <Label>Prompt</Label>
-                <Select>
-                  <SelectTrigger>
-                    <SelectValue placeholder="Selecione um prompt"/>
-                  </SelectTrigger>
-
-                  <SelectContent>
-                    <SelectItem value='title'>Título do Youtube</SelectItem>
-                    <SelectItem value='description'>Descrição do Youtube</SelectItem>
-                  </SelectContent>
-                </Select>
+                <PromptSelect onPromptSelected={setInput} />
               </div>
 
 
@@ -91,6 +109,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className='block text-xs text-muted-foreground italic'>
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
@@ -99,7 +119,7 @@ export function App() {
 
             <Separator/>
 
-            <Button type='submit' className='w-full'>
+            <Button disabled={isLoading} type='submit' className='w-full'>
               Executar
               <Wand2 className='w-4 h-4 ml-2'/>
             </Button>
--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -6,8 +6,24 @@ import { Separator } from "./ui/separator";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from '@ffmpeg/util'
+import { api } from "@/lib/axios";
 
-export function VideoInputForm(){
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!'
+}
+
+interface VideoInputFormProps{
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps){
+    const [status, setStatus] = useState<Status>('waiting')
+
     const [videoFile, setVideoFile] = useState<File | null>(null) 
     //Estado é quando é necessário monitorar uma variável pra quando ela trocar de valor mudar a interface também.
     //VideoFile é a variável, e setVideoFile é a função que é ativada qnd o VideoFile muda.
@@ -82,11 +98,31 @@ export function VideoInputForm(){
             return 
         }
 
+        setStatus('converting')
+
         //converter video em audio
 
         const audioFile = await convertVideoToAudio(videoFile)
 
-        console.log(audioFile, prompt)
+        const data = new FormData()
+
+        data.append('file', audioFile)
+
+        setStatus('uploading')
+
+        const response = await api.post('/videos', data)
+
+        const videoId = response.data.video.id
+
+        setStatus('generating')
+
+        await api.post(`/videos/${videoId}/transcription`, {
+          prompt,
+        })
+
+        setStatus('success')
+
+        props.onVideoUploaded(videoId)
     }
 
     return(
@@ -113,15 +149,25 @@ export function VideoInputForm(){
               <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label>
               <Textarea 
                 ref={promptInputRef}
+                disabled={status != 'waiting'}
                 id='transcription_prompt' 
                 className='h-20 leading-relaxed resize-none'
                 placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'
               />
             </div>
 
-            <Button type='submit' className='w-full'>
-              Carregar Vídeo
-              <Upload className='w-4 h-4 ml-2'/>
+            <Button 
+              data-success={status == 'success'}
+              disabled={status != 'waiting'}
+              type='submit' 
+              className='w-full data-[success=true]:bg-emerald-400'
+            >
+              {status == 'waiting' ? (
+                <>
+                Carregar Vídeo
+                <Upload className='w-4 h-4 ml-2'/>
+                </>
+              ) : statusMessages[status]}
             </Button>
           </form>
     )
